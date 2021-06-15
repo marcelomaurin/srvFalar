f13024ec93757b62b3af55a189c3525e2f7069fb
marcelomaurin <marcelomaurinmartins@gmail.com>
2021-06-15 02:50:15 -0300

    melhorias

--- srv/fala.c
+++ srv/fala.c
@@ -76,7 +76,7 @@ int main(int argc , char *argv[])
     //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8091 );
+    server.sin_port = htons( 7091 );
      
     //Bind
     if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
